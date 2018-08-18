9f063085f3533386e4b2f031e39e7f7d5eed5c0c
Jascha Ehrenreich <jascha@jaeh.at>
2018-08-18 16:13:23 +0200

    formatting

--- transpilers/babel.js
+++ transpilers/babel.js
@@ -6,45 +6,44 @@ const path = require('path')
 const conf = require('../config')()
 const log = require('../log')
 
-const BABEL =
-  ({ buffer, name, resolve, reject }) => {
-    const config = Object.assign(
-      { basedir: conf.HTML_DIR },
-      conf
-    )
+const BABEL = ({ buffer, name, resolve, reject }) => {
+  const config = Object.assign(
+    { basedir: conf.HTML_DIR },
+    conf
+  )
 
-    const babelOptions = Object.assign(
-      {},
-      conf.BABEL,
-      {
-        filename: name,
-        sourceMap: 'both',
-        presets: [
-          'minify',
-          'env',
-          'stage-0',
-          [ 'env', {
-            targets: {
-              browsers: [
-                'last 2 versions',
-                'safari >= 7',
-              ],
-            },
-          }],
-        ],
-      }
-    )
-
-    const result = babel.transform(buffer, babelOptions)
-
-    if (!result.code) {
-      reject(new Error('babel build failed'))
+  const babelOptions = Object.assign(
+    {},
+    conf.BABEL,
+    {
+      filename: name,
+      sourceMap: 'both',
+      presets: [
+        'minify',
+        'env',
+        'stage-0',
+        [ 'env', {
+          targets: {
+            browsers: [
+              'last 2 versions',
+              'safari >= 7',
+            ],
+          },
+        }],
+      ],
     }
+  )
 
-    if (conf.ENV === 'development') {
-      resolve(result.code)
-      return
-    }
+  const result = babel.transform(buffer, babelOptions)
+
+  if (!result.code) {
+    reject(new Error('babel build failed'))
   }
+
+  if (conf.ENV === 'development') {
+    resolve(result.code)
+    return
+  }
+}
 
 module.exports = BABEL
--- transpilers/pug.js
+++ transpilers/pug.js
@@ -6,44 +6,42 @@ const conf = require('../config')()
 const log = require('../log')
 
 // add pug stylus filter
-pug.filters.stylus =
-  (str, options) => {
-    let ret
-    str = str.replace(/\\n/g, '\n')
+pug.filters.stylus = (str, options) => {
+  let ret
+  str = str.replace(/\\n/g, '\n')
 
-    style(str).render(
-      (err, css) => {
-        if (err) {
-          throw err
-        }
-
-        ret = css.replace(/\n/g, '')
-      }
-    )
-
-    return ret
-  }
-
-const PUG =
-  ({ buffer, resolve, reject }) => {
-    const config = Object.assign(
-      { basedir: conf.HTML_DIR },
-      conf
-    )
-
-    pug.render(buffer, config, (err, html) => {
+  style(str).render(
+    (err, css) => {
       if (err) {
-        console.error(err)
-        reject(err)
+        throw err
       }
 
-      if (conf.ENV !== 'production') {
-        resolve(html)
-        return
-      }
+      ret = css.replace(/\n/g, '')
+    }
+  )
 
-      resolve(htmlMinify(html))
-    })
-  }
+  return ret
+}
+
+const PUG = ({ buffer, resolve, reject }) => {
+  const config = Object.assign(
+    { basedir: conf.HTML_DIR },
+    conf
+  )
+
+  pug.render(buffer, config, (err, html) => {
+    if (err) {
+      console.error(err)
+      reject(err)
+    }
+
+    if (conf.ENV !== 'production') {
+      resolve(html)
+      return
+    }
+
+    resolve(htmlMinify(html))
+  })
+}
 
 module.exports = PUG
