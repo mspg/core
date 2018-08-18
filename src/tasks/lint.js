c83d6e684716ce7252f3f7f1e35b9cf3a41d612b
Jascha Ehrenreich <jascha@jaeh.at>
2018-08-19 00:40:35 +0200

    use async await

--- src/tasks/lint.js
+++ src/tasks/lint.js
@@ -1,82 +1,97 @@
+const util = require('util')
 const { exec } = require('child_process')
 const path = require('path')
+const fs = require('fs')
 
 const conf = require('../config')()
 const log = require('../log')
 
-const lintHtml = () =>
-  new Promise((resolve, reject) => {
-    if (!conf.LINT.HTML) {
-      resolve()
-      return
+const xc = util.promisify(exec)
+const exists = util.promisify(fs.exists)
+
+const lintHtml = async () => {
+  if (!conf.LINT.HTML) {
+    return
+  }
+
+  const executable = path.join('node_modules', '.bin', 'pug-lint')
+  let config = path.join(conf.BUILD_DIR, 'pug-lintrc.js')
+
+  try {
+    const configExists = await exists(config)
+    if ( !configExists ) {
+      config = path.join(__dirname, './config/pug-lintrc.js')
     }
+  } 
+  catch(e) {
+    throw e
+  }
 
-    const executable = path.join('node_modules', '.bin', 'pug-lint')
-    const config = path.join(conf.BUILD_DIR, 'pug-lintrc.js')
-    const filesToLint = [
-      path.join(conf.BUNDLE_DIR, '*.html'),
-      path.join(conf.INCLUDES_DIR, 'html', '*.pug'),
-    ].join(' ')
+  const filesToLint = [
+    path.join(conf.BUNDLE_DIR, '*.html'),
+    path.join(conf.INCLUDES_DIR, 'html', '*.pug'),
+  ].join(' ')
 
-    const cmd = `${executable} --config ${config} ${filesToLint}`
+  const cmd = `${executable} --config ${config} ${filesToLint}`
 
-    exec(cmd, (err, res) => {
-      if (err) {
-        reject(err)
+  try {
+    const res = await xc(cmd)
+
+    log('pug-lint results:', res)
+
+    return res
+  }
+  catch(e) {
+    throw e
+  }
+}
+
+const lintCss = async () => {
+  if (!conf.LINT.CSS) {
+    return
+  }
+
+  const executable = path.join('node_modules', '.bin', 'stylint')
+
+  const configPath = path.join(__dirname, 'config', '.stylintrc')
+  const filesToLint = [path.join(conf.BUNDLE_DIR, '*.css'), path.join(conf.INCLUDES_DIR, 'css')]
+
+  let timesReturned = 0
+
+  await Promise.all(filesToLint.map(async dir => {
+    const cmd = `${executable} --config ${configPath} ${dir}`
+    // console.log('exec :', cmd)
+    
+    try {
+      const { stdout } = await xc(cmd)
+  
+      log('stylint results:', stdout)
+
+      timesReturned += 1
+      if (timesReturned >= filesToLint.length) {
+        return
       }
-
-      log('pug-lint results:', res)
-      resolve()
-    })
-  })
-
-const lintCss = () =>
-  new Promise((resolve, reject) => {
-    if (!conf.LINT.CSS) {
-      resolve()
-      return
     }
-
-    const executable = path.join('node_modules', '.bin', 'stylint')
-
-    const configPath = path.join(__dirname, 'config', '.stylintrc')
-    const filesToLint = [path.join(conf.BUNDLE_DIR, '*.css'), path.join(conf.INCLUDES_DIR, 'css')]
-
-    let timesReturned = 0
-
-    filesToLint.forEach(dir => {
-      const cmd = `${executable} --config ${configPath} ${dir}`
-
-      // console.log('exec :', cmd)
-
-      exec(cmd, (err, stdout) => {
-        if (err) {
-          reject(err)
-          return
-        }
-        log('stylint results:', stdout)
-
-        timesReturned += 1
-        if (timesReturned >= filesToLint.length) {
-          log('stylint finished')
-          resolve()
-        }
-      })
-    })
-  })
-
-const lint = () =>
-  new Promise((resolve, reject) => {
-    if (!conf.LINT.HTML && !conf.LINT.CSS) {
-      resolve()
-      return
+    catch(e) {
+      throw e
     }
+  }))
+  log('stylint finished')
+}
 
-    return lintHtml()
-      .then(lintCss)
-      .then(() => log('Linting completed'))
-      .then(resolve)
-      .catch(reject)
-  })
+const lint = async () => {
+  if (!conf.LINT.HTML && !conf.LINT.CSS) {
+    return
+  }
+
+  try {
+    await lintHtml()
+    await lintCss()
+    log('Linting completed')
+  }
+  catch(e) {
+    throw e
+  }
+}
 
 module.exports = lint
