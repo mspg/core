02b8c6c8f50692b84323eb39935fee94d87716ea
Jascha <jascha@jaeh.at>
2018-11-02 07:08:37 +0100

    log is not colored, but also does not exec child processes

--- src/log.js
+++ src/log.js
@@ -1,19 +1,9 @@
-const { exec } = require('child_process')
 const log = (...msgs) => console.log(...msgs)
 
-// note the double string declarations!
-const red = '`tput setaf 1`'
-const green = '`tput setaf 2`'
-const yellow = '`tput setaf 3`'
+log.success = (...msgs) => console.log('SUCCESS', ...msgs)
 
-const print = (e, stdout) => console.log(stdout)
+log.warn = (...msgs) => console.log('WARN', ...msgs)
 
-const stringify = (...msgs) => (msgs.length === 1 ? msgs[0] : JSON.stringify(msgs))
-
-log.success = (...msgs) => exec(`echo ${green} "${stringify(msgs)}"`, print)
-
-log.warn = (...msgs) => exec(`echo ${yellow} "${stringify(msgs)}"`, print)
-
-log.error = (...msgs) => exec(`echo ${red} "${stringify(msgs)}"`, print)
+log.error = (...msgs) => console.log('ERROR', ...msgs)
 
 module.exports = log
