17ba777b3d7d43ce2629f65085cef57b1a5f51e0
Jascha <jascha@jaeh.at>
2018-11-05 20:17:01 +0100

    only log.info if VERBOSE is set

--- src/log.js
+++ src/log.js
@@ -1,3 +1,5 @@
+const conf = require('./config.js')()
+
 const log = (...msgs) => console.log(...msgs)
 
 log.success = (...msgs) => console.log('SUCCESS', ...msgs)
@@ -5,5 +7,7 @@ log.success = (...msgs) => console.log('SUCCESS', ...msgs)
 log.warn = (...msgs) => console.log('WARN', ...msgs)
 
 log.error = (...msgs) => console.log('ERROR', ...msgs)
+
+log.info = (...msgs) => conf.VERBOSE && console.log('INFO', ...msgs)
 
 module.exports = log
