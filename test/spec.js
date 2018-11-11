06c64d54f3289fb55c79653aa5b2165fc0cd7d21
Jascha <jascha@jaeh.at>
2018-11-11 04:38:39 +0100

    add minimal tests

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,13 @@
 const { is } = require('@magic/test')
 const config = require('../src/config')
+const localConfig = require('../config')
 
 module.exports = [
   { fn: () => config, expect: is.object, info: 'calling config returns an object' },
+  { fn: () => config.CWD, expect: is.string, info: 'config.CWD is a string' },
+  {
+    fn: () => config.CWD,
+    expect: localConfig.CWD,
+    info: 'config.CWD equals the cwd in the cwd/config.js file'
+  },
 ]
