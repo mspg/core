1730a2e1791c4e5470a11bc18554405aca1777dc
Jascha <jascha@jaeh.at>
2018-11-11 03:28:57 +0100

    update spec since config is now an object

--- test/spec.js
+++ test/spec.js
@@ -2,6 +2,5 @@ const { is } = require('@magic/test')
 const config = require('../src/config')
 
 module.exports = [
-  { fn: () => config, expect: is.function, info: 'config exports a function' },
-  { fn: () => config(), expect: is.object, info: 'calling config returns an object' },
+  { fn: () => config, expect: is.object, info: 'calling config returns an object' },
 ]
