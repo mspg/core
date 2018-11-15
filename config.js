8ffa3c54f82e0381df0fcbd87381fe625ebe338e
Jascha <jascha@jaeh.at>
2018-11-15 22:51:08 +0100

    add out_dir and comments

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
 module.exports = {
+  // files get loaded from example/src and example/includes
   CWD: path.join(__dirname, 'example'),
+  // and published in example/publish
+  OUT_DIR: path.join(__dirname, 'example', 'public'),
 }
