367d7000b57855d0d4daff63852c945a3105e496
Jascha <jascha@jaeh.at>
2018-11-15 23:54:51 +0100

    clean outDir, add logs

--- src/tasks/publish.js
+++ src/tasks/publish.js
@@ -13,21 +13,22 @@ const publish = async () => {
     return
   }
 
-  const outDirArray = OUT_DIR.split('/')
-  const outDir = outDirArray[outDirArray.length - 1]
+  const outDir = OUT_DIR.replace(`${process.cwd()}/`, '')
 
   const cmdPrefix = `--prefix=${outDir}`
   const cmdOnto = `--onto=${GIT_ORIGIN}/${GIT_BRANCH}`
   const cmdArgv = `${cmdPrefix} ${cmdOnto}`
   const cmd = `git subtree split ${cmdArgv}`
 
-  log('exec', cmd)
 
   try {
+    log('prepare', cmd)
     const { stdout } = await xc(cmd)
     const id = stdout.trim()
+    log('prepare done', id)
 
     const cmd2 = `git push ${GIT_ORIGIN} ${id.trim()}:${GIT_BRANCH}`
+    log('push', cmd2)
     await xc(cmd2)
     log.success('publish finished')
   } catch (e) {
