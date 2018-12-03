f0c0f012c86d76ffa2d8c631e64a26cf4d94760e
Jascha <jascha@jaeh.at>
2018-12-03 13:28:52 +0100

    use includes instead of indexOf, refactor LINT

--- src/config.js
+++ src/config.js
@@ -20,26 +20,30 @@ config.OUT_DIR = path.join(config.CWD, 'public')
 config.CSS_DIR = path.join(config.INCLUDES_DIR, 'css')
 config.HTML_DIR = path.join(config.INCLUDES_DIR, 'html')
 config.JS_DIR = path.join(config.INCLUDES_DIR, 'js')
-config.WATCH = argv.indexOf('watch') > -1
-config.SERVE = argv.indexOf('serve') > -1
+config.WATCH = argv.includes('watch')
+config.SERVE = argv.includes('serve')
 config.GIT_ORIGIN = 'origin'
 config.GIT_BRANCH = 'gh-pages'
 config.IGNORE_EXTENSIONS = []
-config.VERBOSE = argv.indexOf('verbose') > -1
+config.VERBOSE = argv.includes('verbose')
 
-const LINT = {
-  HTML: argv.includes('lint') && argv.includes('html'),
-  CSS: argv.includes('lint') && argv.includes('css'),
-  ...config.LINT,
+const LINT = config.LINT || { 
+  HTML: false,
+  CSS: false,
+}
+
+config.LINT = {
+  HTML: LINT.HTML || argv.includes('lint') && argv.includes('html'),
+  CSS: LINT.CSS || argv.includes('lint') && argv.includes('css'),
 }
 
 config.TASKS = {
-  CLEAN: argv.indexOf('clean') > -1 || config.ENV === 'production',
-  BUILD: argv.indexOf('build') > -1,
-  LINT: argv.indexOf('lint') > -1,
-  PUBLISH: argv.indexOf('publish') > -1,
-  ZIP: argv.indexOf('zip') > -1,
-  CONNECT: argv.indexOf('connect') > -1,
+  CLEAN: argv.includes('clean') || config.ENV === 'production',
+  BUILD: argv.includes('build'),
+  LINT: argv.includes('lint'),
+  PUBLISH: argv.includes('publish'),
+  ZIP: argv.includes('zip'),
+  CONNECT: argv.includes('connect'),
 }
 
 config.MINIFY = {
