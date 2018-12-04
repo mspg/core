2bc4e22553f65892ac8d84b7c7ced5cd806c784e
Jascha <jascha@jaeh.at>
2018-12-04 22:57:04 +0100

    replace WEB_ROOT with path before running transpilers

--- src/lib/fs/transpileFile.js
+++ src/lib/fs/transpileFile.js
@@ -3,12 +3,22 @@ const getFileType = require('./getFileType')
 
 const conf = require('../../config')
 
+const transpileHTML = html =>
+  html
+    .replace(/{{ /gi, '{{')
+    .replace(/ }}/gi, '}}')
+    .replace(/{{WEB_ROOT}}/gi, conf.WEB_ROOT)
+
 const transpileFile = async (file) => {
   let { name, buffer } = file
   const type = getFileType(name)
   if (conf.IGNORE_EXTENSIONS.indexOf(type) > -1) {
     log.info('File ignored by extension', name)
     return
+  }
+
+  if (type === 'html') {
+    buffer = transpileHTML(buffer.toString())
   }
 
   if (!is.empty(conf.TRANSPILERS)) {
