650e58391c2f1e6bd3027d354dbc65bacbea00c1
Jascha <jascha@jaeh.at>
2018-12-05 18:46:43 +0100

    comments

--- src/run.js
+++ src/run.js
@@ -2,16 +2,22 @@ const tasks = require('./tasks')
 
 const run = async () => {
   try {
+    // connects this installation to github/gitlab remote
     await tasks.connect()
 
+    // delete public dir
     await tasks.clean()
+
+    // build html/css/js and other files, run transpiler plugins
     await tasks.build()
 
     // disable until using csslint and htmllint instead of puglint and stylint
     // await tasks.lint()
 
+    // use zopfli to compress files
     await tasks.zip()
 
+    // publish public/ dir to github/gitlab
     await tasks.publish()
   } catch (e) {
     throw e
