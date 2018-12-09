cbb34f64c0c051a2e3204a93ba9510c0db03fbd6
Jascha <jascha@jaeh.at>
2018-12-09 08:31:09 +0100

    add tests for addTrailingSlash

---
+++ test/lib/addTrailingSlash.js
@@ -0,0 +1,7 @@
+const addTrailingSlash = require('../../src/lib/addTrailingSlash')
+
+module.exports = [
+  { fn: addTrailingSlash(''), expect: '/', info: 'empty string turns into /' },
+  { fn: addTrailingSlash('/test'), expect: '/test/', info: '/ gets appended if missing' },
+  { fn: addTrailingSlash('/test/'), expect: '/test/', info: '/ does not get added if exists' },
+]
