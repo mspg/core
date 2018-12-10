3ddeb3b380f97f7b97aa9e424878ba80dd698c08
Jascha <jascha@jaeh.at>
2018-12-10 17:17:47 +0100

    add test

--- test/lib/addTrailingSlash.js
+++ test/lib/addTrailingSlash.js
@@ -4,4 +4,5 @@ module.exports = [
   { fn: addTrailingSlash(''), expect: '/', info: 'empty string turns into /' },
   { fn: addTrailingSlash('/test'), expect: '/test/', info: '/ gets appended if missing' },
   { fn: addTrailingSlash('/test/'), expect: '/test/', info: '/ does not get added if exists' },
+  { fn: addTrailingSlash('/'), expect: '/', info: '/ does not get added if argument is / already' },
 ]
