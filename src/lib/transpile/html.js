2869cd545af7fccf65a8e104e83afa40224bd148
Jascha <jascha@jaeh.at>
2018-12-10 18:08:03 +0100

    fix regexes. still weird behaviour, but works

--- src/lib/transpile/html.js
+++ src/lib/transpile/html.js
@@ -3,13 +3,13 @@ const config = require('../../config')
 
 const { WEB_ROOT } = config
 
-const transpileOpenRegex = /\"\s+{\s+{\s+/gim
-const transpileCloseRegex = /\s+}\s+}\s+\"/gim
+const transpileOpenRegex = /"\s*{\s*{\s*/gim
+const transpileCloseRegex = /\s*}\s*}\s*/gim
 
 const transpileHTML = html =>
   html
     .replace(transpileOpenRegex, '"{{')
-    .replace(transpileCloseRegex, '}}"')
-    .replace(/{{WEB_ROOT}}/gi, addTrailingSlash(WEB_ROOT))
+    .replace(transpileCloseRegex, '}}')
+    .replace(/\s*{\s*{\s*WEB_ROOT\s*}\s*}\s*/gim, addTrailingSlash(WEB_ROOT))
 
 module.exports = transpileHTML
