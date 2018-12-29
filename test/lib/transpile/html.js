00170e19b3c627c7656546959de108826dae4ede
Jascha <jascha@jaeh.at>
2018-12-29 22:52:10 +0100

    update tests to test DIR replace in transpilehtml

--- test/lib/transpile/html.js
+++ test/lib/transpile/html.js
@@ -11,14 +11,19 @@ module.exports = [
     info: 'errors on invalid html string argument',
   },
   {
-    fn: transpileHTML({ name: 'test', buffer: '" { { WEB_ROOT } } "' }),
+    fn: transpileHTML({ name: '/deep/index.html', buffer: '" { { WEB_ROOT } } "' }),
     expect: t => t.startsWith('"/') && t.endsWith('/"'),
     info: 'correctly transforms {{WEB_ROOT}}',
   },
   {
-    fn: transpileHTML({ name: 'test', buffer: '" { { WEB_ROOT } } "' }),
+    fn: transpileHTML({ name: '/index.html', buffer: '" { { WEB_ROOT } } "' }),
     expect: t => t.includes(WEB_ROOT),
     info: 'correctly transforms {{WEB_ROOT}}',
+  },
+  {
+    fn: transpileHTML({ name: '/testing/dir/file.html', buffer: '" { { DIR } } "' }),
+    expect: t => t.includes('"/testing/dir/"'),
+    info: 'correctly transforms {{DIR}}',
   },
   {
     fn: '" { { test'.replace(transpileHTML.regex.open, '{{'),
