0a9e23ed2cc63a66d61e15135b8d2baf4b05d7d4
Jascha <jascha@jaeh.at>
2019-03-14 04:22:37 +0100

    make it possible to overwrite all directories, config.OUT_DIR outside config.CWD is possible

--- src/config.js
+++ src/config.js
@@ -10,22 +10,36 @@ if (fs.existsSync(configPath)) {
   config = require(configPath)
 }
 
+const maybePrefixPath = (p, CWD) => {
+  if (!p.startsWith(CWD)) {
+    p = path.join(CWD, p)
+  }
+
+  return p
+}
+
 // config variables
-config.CWD = config.CWD || process.cwd()
 config.ENV = process.env.NODE_ENV || 'development'
-config.BUILD_DIR = path.join(config.CWD, 'build')
-config.INCLUDES_DIR = path.join(config.CWD, 'includes')
-config.BUNDLE_DIR = path.join(config.CWD, 'src')
-config.OUT_DIR = path.join(config.CWD, 'public')
-config.CSS_DIR = path.join(config.INCLUDES_DIR, 'css')
-config.HTML_DIR = path.join(config.INCLUDES_DIR, 'html')
-config.JS_DIR = path.join(config.INCLUDES_DIR, 'js')
+
+config.CWD = config.CWD || process.cwd()
+
+config.BUILD_DIR = maybePrefixPath(config.BUILD_DIR || 'build', config.CWD)
+config.INCLUDES_DIR = maybePrefixPath(config.INCLUDES_DIR || 'includes', config.CWD)
+config.BUNDLE_DIR = maybePrefixPath(config.BUNDLE_DIR || 'src', config.CWD)
+config.OUT_DIR = config.OUT_DIR || path.join(config.CWD, 'public')
+config.CSS_DIR = maybePrefixPath(config.CSS_DIR || 'css', config.INCLUDES_DIR)
+config.HTML_DIR = maybePrefixPath(config.HTML_DIR || 'html', config.INCLUDES_DIR)
+config.JS_DIR = maybePrefixPath(config.JS_DIR || 'js', config.INCLUDES_DIR)
+
 config.WATCH = argv.includes('watch')
 config.SERVE = argv.includes('serve')
+
 config.GIT_ORIGIN = 'origin'
 config.GIT_BRANCH = 'gh-pages'
+
 config.IGNORE_EXTENSIONS = []
 config.VERBOSE = argv.includes('verbose')
+
 
 config.IMAGE_EXTENSIONS = ['gif', 'jpg', 'jpeg', 'png', 'webp', 'jp2']
 config.MAX_IMAGE_WIDTH = config.MAX_IMAGE_WIDTH || 2000
