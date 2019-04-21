2cca7f79b0825ea2727f2f80ae3c830766a9c8f2
Jascha <jascha@jaeh.at>
2019-04-21 22:35:43 +0200

    move public to docs

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   // files get loaded from example/src and example/includes
   CWD: path.join(__dirname, 'example'),
   // and published in example/publish
-  OUT_DIR: path.join(__dirname, 'example', 'public'),
+  OUT_DIR: 'docs',
   // root of the homepage, useful if you are using username.github.io/repo style urls.
   // trailing slash gets added if missing
   WEB_ROOT: '/core',
