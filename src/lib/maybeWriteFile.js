1922933b815d642b146c5259e2eca6c30c7ea340
jaeh <jascha@jaeh.at>
2021-03-19 04:12:32 +0100

    maybeWriteFile: check if image changed before writing images. write webp files

--- src/lib/maybeWriteFile.js
+++ src/lib/maybeWriteFile.js
@@ -1,5 +1,8 @@
+import crypto from 'crypto'
+
 import log from '@magic/log'
 import is from '@magic/types'
+import fs from '@magic/fs'
 
 import transpileFile from './transpileFile.js'
 import minifyFile from './minifyFile.js'
@@ -30,27 +33,47 @@ const maybeWriteFile = (watchedFiles, conf) => async name => {
     const bundle = await transpileFile({ name, buffer }, conf)
 
     if (bundle) {
-      const minified = await minifyFile({ name, bundle }, conf)
 
-      await write({ buffer, bundle: minified, out }, conf)
-
-      const images = ['jpg', 'png', 'gif']
+      const images = ['jpg', 'jpeg', 'png', 'gif']
 
       const isImage = images.some(type => name.endsWith(type.toLowerCase()) || name.endsWith(type.toUpperCase()))
 
       // save all images as webp too
       if (isImage) {
-        const webpOut = out.replace(/\.(jpg|png|gif)$/gim, '.webp')
+        const pubImage = name.replace(conf.BUNDLE_DIR, conf.OUT_DIR)
 
-        // pages: -1 saves all frames of animated gifs
-        await sharp(name, { pages: -1 }).toFile(webpOut)
+        let pubImageContent
+
+        const minified = await minifyFile({ name, bundle }, conf)
+
+        if (await fs.exists(pubImage)) {
+          pubImageContent = await getFileContent({ name: pubImage }, conf)
+        }
+
+        if (!pubImageContent || Buffer.compare(pubImageContent.buffer, minified) !== 0) {
+          const webpOut = out.replace(/\.(jpg|png|gif)$/gim, '.webp')
+
+          await write({ buffer, bundle: minified, out }, conf)
+
+          // pages: -1 saves all frames of animated gifs
+          await sharp(name, { pages: -1 }).toFile(webpOut)
+          log.info('writeFile', webpOut)
+
+          watchedFiles[name].content = minified
+          return minified
+        }
+
+      } else {
+        const minified = await minifyFile({ name, bundle }, conf)
+
+        await write({ buffer, bundle: minified, out }, conf)
+
+        watchedFiles[name].content = minified
+
+        // log.timeEnd(timeIndex)
+
+        return minified
       }
-
-      watchedFiles[name].content = minified
-
-      // log.timeEnd(timeIndex)
-
-      return minified
     }
   } catch (e) {
     throw e
