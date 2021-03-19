e016adaf6fcb7b30edb276e631dbcc19b77640a1
jaeh <jascha@jaeh.at>
2021-03-19 01:12:53 +0100

    config: use process.cwd instead of __dirname

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 import path from 'path'
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const cwd = process.cwd()
 
 export default {
   // files get loaded from example/src and example/includes
-  CWD: path.join(__dirname, 'example'),
+  CWD: path.join(cwd, 'example'),
   // and published in example/publish
   OUT_DIR: 'docs',
   // root of the homepage, useful if you are using username.github.io/repo style urls.
